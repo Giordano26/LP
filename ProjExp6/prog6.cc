fcfdc161485db1b7e8c201fa53d2adc9504b2db0
Stefano <smfgiordano@gmail.com>
2020-08-05 13:23:47 -0300

    05/08/2020

--- ProjExp6/prog6.cc
+++ ProjExp6/prog6.cc
@@ -1,20 +1,20 @@
-//Prog 6 - cabeçalho, f de 0 a 1000 de 10 em 10, inverter colunas da tabela, adicionado kelvin
+//Prog 6 - cabeçalho, fahrenheit de 0 a 1000 de 10 em 10, inverter colunas da tabela, adicionado kelvin
 //Stefano Giordano - 05/08/2020
 
 #include <iostream>
 
 int main(){
-    float fahr, celsius, kelvin;
-    int inicio, fim, incr;
+    float fahr, celsius, kelvin; //inicialização das variaveis para as temperaturas 
+    int inicio, fim, incr; //inicialização das variaveis de sentinela 
     inicio = 0; fim = 1000; incr = 10;
     fahr = inicio;
-    printf("=========================================\n");
+    printf("=========================================\n"); //cabeçalho
     printf("Celsius        Fahrenheint       Kelvin\n");
     printf("=========================================\n");
     while(fahr <= fim){
-        celsius = 5*(fahr-32)/9;
-        kelvin = celsius + 273.15;
-        printf("%f\t%f\t%f\n",celsius,fahr,kelvin);
-        fahr += incr;
+        celsius = 5*(fahr-32)/9; //formula fahrenheit p/ celsius
+        kelvin = celsius + 273.15; //formula celsius para kelvin
+        printf("%f\t%f\t%f\n",celsius,fahr,kelvin); //tabulação das temperaturas calculadas
+        fahr += incr; //incremento do loop
 }
 }
